eb98a11a0b820c8e87f6d1474aba95b6c396ca1e
Hugh Graham <hugh.a.graham@gmail.com>
2023-07-27 00:05:36 +0100

    improve print method for find class

--- R/chewie-print.r
+++ R/chewie-print.r
@@ -1,11 +1,12 @@
 #' @title generic function for printing chewie objects
 #' @param x chewie object to print
 #' @noRd
-chewie_print <- function(x) {
+chewie_print <- function(x, ...) {
     opts <- options(
         datatable.print.class = getOption("chewie.print.class"),
         datatable.print.keys = getOption("chewie.print.keys"),
         datatable.print.topn = getOption("chewie.print.topn"),
+        datatable.print.nrows = getOption("chewie.print.nrows"),
         datatable.print.trunc.cols = getOption("chewie.print.trunc.cols"),
         datatable.prettyprint.char = getOption("chewie.prettyprint.char"),
         width = getOption("chewie.print.width")
@@ -14,5 +15,11 @@ chewie_print <- function(x) {
     on.exit(options(opts))
 
     cli::cli_h1(class(x)[1])
-    print(data.table::as.data.table(x))
+    cli::cli_inform(c("*" = paste0(
+        "  ",
+        chew_bold_green(attributes(x)$gedi_product)
+    )))
+    print(data.table::as.data.table(x), ...)
+    cli::cli_h1("")
+    invisible()
 }
