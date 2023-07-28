64bbb2e4d3090cb28c0f1503fc258fed009ea34e
Hugh Graham <hugh.a.graham@gmail.com>
2023-07-28 19:37:22 +0100

    more print options

--- R/chewie-print.r
+++ R/chewie-print.r
@@ -3,10 +3,12 @@
 #' @noRd
 chewie_print <- function(x, ...) {
     opts <- options(
-        datatable.print.class = getOption("chewie.print.class"),
-        datatable.print.keys = getOption("chewie.print.keys"),
         datatable.print.topn = getOption("chewie.print.topn"),
         datatable.print.nrows = getOption("chewie.print.nrows"),
+        datatable.print.class = getOption("chewie.print.class"),
+        data.table.print.row.names = getOption("chewie.print.rownames"),
+        data.table.print.col.names = getOption("chewie.print.colnames"),
+        datatable.print.keys = getOption("chewie.print.keys"),
         datatable.print.trunc.cols = getOption("chewie.print.trunc.cols"),
         datatable.prettyprint.char = getOption("chewie.prettyprint.char"),
         width = getOption("chewie.print.width")
