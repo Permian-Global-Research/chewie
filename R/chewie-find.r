5a0d74628b2eeda9b208ba23b5f79218b7310f3e
Hugh Graham <hugh.a.graham@gmail.com>
2023-07-28 19:37:56 +0100

    remove versions update class now chewie.find and a producrt class

--- R/chewie-find.r
+++ R/chewie-find.r
@@ -19,17 +19,17 @@
 chewie_find <- function(
     x,
     gedi_product = c("1B", "2A", "2B"),
-    gedi_version = c("v2", "v1"),
     date_start = NULL,
     date_end = NULL,
     intersects = TRUE) {
+    assert_gedi_product(gedi_product)
+
     bbox <- paste(chewie_bbox(x), collapse = ",")
 
     date_range <- build_date_range(date_start, date_end)
 
     request_url <- build_req_url(
         gedi_product[1],
-        gedi_version[1],
         bbox,
         date_range
     )
@@ -56,17 +56,16 @@ chewie_find <- function(
     }
 
     attr(sf_list, "aoi") <- get_spat_outline(x)
-    attr(sf_list, "gedi_product") <- paste("GEDI",
-        gedi_product[1], gedi_version[1],
-        sep = "-"
+    attr(sf_list, "gedi_product") <- paste0("GEDI-", gedi_product[1])
+    attr(sf_list, "class") <- c(
+        "chewie.find",
+        paste("chewie.find", "gedi", gedi_product[1], sep = "."),
+        class(sf_list)
     )
-    attr(sf_list, "class") <- c("chewie.find", class(sf_list))
     return(sf_list)
 }
 
 request_gedi <- function(.url, .page) {
-    # Append fetched granules to granules_href
-    # recursively, for each page (max 2000 per page)
     response <- curl::curl_fetch_memory(paste0(
         .url,
         "&pageNum=",
@@ -110,24 +109,21 @@ build_sf_set <- function(gedi_response) {
 }
 
 
-gedi_code_lookup <- function(.gprod, .gver) {
-    switch(paste0("G", .gprod, .gver),
-        G1Bv1 = "C1656765475-LPDAAC_ECS",
-        G2Av1 = "C1656766463-LPDAAC_ECS",
-        G2Bv1 = "C1656767133-LPDAAC_ECS",
-        G1Bv2 = "C1908344278-LPDAAC_ECS",
-        G2Av2 = "C1908348134-LPDAAC_ECS",
-        G2Bv2 = "C1908350066-LPDAAC_ECS",
+gedi_code_lookup <- function(.gprod) {
+    switch(paste0("G", .gprod),
+        G1B = "C1908344278-LPDAAC_ECS",
+        G2A = "C1908348134-LPDAAC_ECS",
+        G2B = "C1908350066-LPDAAC_ECS",
         abort_gedi_opts()
     )
 }
 
 
-build_req_url <- function(.gprod, .gver, .bbox, .dr) {
+build_req_url <- function(.gprod, .bbox, .dr) {
     req_url <- paste0(
         "https://cmr.earthdata.nasa.gov/search/granules.json?",
         "pretty=true&provider=LPDAAC_ECS&page_size=2000&concept_id=",
-        gedi_code_lookup(.gprod, .gver),
+        gedi_code_lookup(.gprod),
         "&bounding_box=",
         .bbox
     )
@@ -167,13 +163,17 @@ build_date_range <- function(.sd, .ed) {
 #' objects. Essentially a wrapper for `print.data.table` with some custom
 #' options. See details for options.
 #' @param x chewie.find object to print
-#' @param ... additional arguments passed to `print.data.table`
+#' @param ... arguments passed to
+#' \code{\link[data.table:print.data.table]{data.table::print.data.table}}
+#' @rdname chewie-print
+#' @family print
 #' @export
 #' @details Edit the following options to change the default printing behaviour
 #' of `chewie.find` objects: `chewie.print.class`, `chewie.print.keys`,
 #' `chewie.print.topn`, `chewie.print.nrows`, `chewie.print.trunc.cols`,
 #' `chewie.prettyprint.char`, `chewie.print.width`. Additonal options for
 #' `print.data.table` can also be set to alter the print layout.
+#'
 print.chewie.find <- function(x, ...) {
     chewie_print(x, ...)
 }
