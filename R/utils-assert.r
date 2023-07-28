8553e1b10623a467b4d31195464b1e5beafaebff
Hugh Graham <hugh.a.graham@gmail.com>
2023-07-28 19:37:08 +0100

    add gedi product asser

--- R/utils-assert.r
+++ R/utils-assert.r
@@ -7,3 +7,12 @@ chk_pkg <- function(pkg, abort_fun) {
         abort_fun()
     }
 }
+
+
+assert_gedi_product <- function(x) {
+    if (x %in% c("1B", "2A", "2B")) {
+        return(invisible())
+    } else {
+        abort_gedi_opts()
+    }
+}
