6abf4db14a632666c3708e19657a441eab88e7a6
Hugh Graham <hugh.a.graham@gmail.com>
2023-11-10 14:21:14 +0000

    fix issue #5

--- R/chewie_scan.r
+++ R/chewie_scan.r
@@ -4,62 +4,72 @@
 #' @param x A `chewie.find` object.
 #' @noRd
 chewie_scan <- function(x) {
-    gedi_product <- find_gedi_product(x)
+  gedi_product <- find_gedi_product(x)
 
-    parquet_dir <- file.path(
-        getOption("chewie.parquet.cache"),
-        gedi_product
+  parquet_dir <- file.path(
+    getOption("chewie.parquet.cache"),
+    gedi_product
+  )
+
+  nothing_here <- function(x) {
+    reclass <- class(x)
+    x$cached <- FALSE
+    class(x) <- reclass
+    return(x)
+  }
+
+  # get paths for existing folders
+  swath_dirs <- list.dirs(parquet_dir, recursive = FALSE)
+  if (length(swath_dirs) == 0) {
+    return(nothing_here(x))
+  }
+  # extract ids from existing folders
+  all_swaths <- sub(".*\\=", "", x = basename(swath_dirs))
+  # get ids for swaths that are not in the cache
+  req_swaths <- x$id[!x$id %in% all_swaths]
+  if (length(req_swaths) == nrow(x)) {
+    return(nothing_here(x))
+  }
+
+  # get paths for swaths that are in the cache
+  cached_swaths <- x$id[x$id %in% all_swaths]
+  # check if there are parquet files in the folders
+  cached_bool <- purrr::map_vec(
+    swath_dirs,
+    ~ length(list.files(.x, pattern = ".*\\.parquet$")) > 0
+  )
+
+  cache_tab <- data.frame(
+    id = cached_swaths,
+    cached = cached_bool
+  )
+
+  # save classes and attributes now before transformation.
+  x_class <- class(x)
+  aoi_attr <- attr(x, "aoi")
+  gedi_product_attr <- attr(x, "gedi_product")
+  intersects_attr <- attr(x, "intersects")
+
+  # drop `cached` column if it exists in `x`
+  # left join cache tab with x
+  cache_tab <- x |>
+    dplyr::select(!dplyr::any_of("cached")) |>
+    dplyr::left_join(
+      cache_tab,
+      by = "id"
+    ) |>
+    dplyr::mutate(
+      cached = dplyr::case_when(
+        is.na(cached) ~ FALSE,
+        TRUE ~ cached
+      )
     )
 
-    nothing_here <- function(x) {
-        reclass <- class(x)
-        x$cached <- FALSE
-        class(x) <- reclass
-        return(x)
-    }
+  # reassign classes and attributes
+  class(cache_tab) <- x_class
+  attr(cache_tab, "aoi") <- aoi_attr
+  attr(cache_tab, "gedi_product") <- gedi_product_attr
+  attr(cache_tab, "intersects") <- intersects_attr
 
-    # get paths for existing folders
-    swath_dirs <- list.dirs(parquet_dir, recursive = FALSE)
-    if (length(swath_dirs) == 0) {
-        return(nothing_here(x))
-    }
-    # extract ids from existing folders
-    all_swaths <- sub(".*\\=", "", x = basename(swath_dirs))
-    # get ids for swaths that are not in the cache
-    req_swaths <- x$id[!x$id %in% all_swaths]
-    if (length(req_swaths) == nrow(x)) {
-        return(nothing_here(x))
-    }
-
-    # get paths for swaths that are in the cache
-    cached_swaths <- x$id[x$id %in% all_swaths]
-    # check if there are parquet files in the folders
-    cached <- purrr::map_vec(
-        swath_dirs,
-        ~ length(list.files(.x, pattern = ".*\\.parquet$")) > 0
-    )
-
-    cache_tab <- data.frame(
-        id = cached_swaths,
-        cached = cached
-    )
-
-    # left join cache tab with x
-    cache_tab <- dplyr::left_join(
-        x,
-        cache_tab,
-        by = "id"
-    ) |> dplyr::mutate(
-        cached = dplyr::case_when(
-            is.na(cached) ~ FALSE,
-            TRUE ~ cached
-        )
-    )
-
-    class(cache_tab) <- class(x)
-    attr(cache_tab, "aoi") <- attr(x, "aoi")
-    attr(cache_tab, "gedi_product") <- attr(x, "gedi_product")
-    attr(cache_tab, "intersects") <- attr(x, "intersects")
-
-    return(cache_tab)
+  return(cache_tab)
 }
