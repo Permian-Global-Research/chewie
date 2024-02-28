89f6ba9b8a9cb85888ee94f1089e651d52320876
Hugh Graham <hugh.a.graham@gmail.com>
2024-02-28 10:22:18 +0000

    improve health check to asses cache sizes etc

--- R/chewie-print.r
+++ R/chewie-print.r
@@ -1,27 +1,29 @@
 #' @title generic function for printing chewie objects
 #' @param x chewie object to print
 #' @noRd
-chewie_print <- function(x, ...) {
-    opts <- options(
-        datatable.print.topn = getOption("chewie.print.topn"),
-        datatable.print.nrows = getOption("chewie.print.nrows"),
-        datatable.print.class = getOption("chewie.print.class"),
-        data.table.print.row.names = getOption("chewie.print.rownames"),
-        data.table.print.col.names = getOption("chewie.print.colnames"),
-        datatable.print.keys = getOption("chewie.print.keys"),
-        datatable.print.trunc.cols = getOption("chewie.print.trunc.cols"),
-        datatable.prettyprint.char = getOption("chewie.prettyprint.char"),
-        width = getOption("chewie.print.width")
-    )
+chewie_print <- function(
+    x,
+    .printclass = getOption("chewie.print.class"),
+    ...) {
+  opts <- options(
+    datatable.print.topn = getOption("chewie.print.topn"),
+    datatable.print.nrows = getOption("chewie.print.nrows"),
+    datatable.print.class = .printclass,
+    data.table.print.col.names = getOption("chewie.print.colnames"),
+    datatable.print.keys = getOption("chewie.print.keys"),
+    datatable.print.trunc.cols = getOption("chewie.print.trunc.cols"),
+    datatable.prettyprint.char = getOption("chewie.prettyprint.char"),
+    width = getOption("chewie.print.width")
+  )
 
-    on.exit(options(opts))
+  on.exit(options(opts))
 
-    cli::cli_h1(class(x)[1])
-    cli::cli_inform(c("*" = paste0(
-        "  ",
-        chew_bold_green(attributes(x)$gedi_product)
-    )))
-    print(data.table::as.data.table(x), ...)
-    cli::cli_h1("")
-    invisible()
+  cli::cli_h1(class(x)[1])
+  cli::cli_inform(c("*" = paste0(
+    "  ",
+    chew_bold_green(attributes(x)$gedi_product)
+  )))
+  print(data.table::as.data.table(x), ...)
+  cli::cli_h1("")
+  invisible()
 }
