964b80626a8da2043cb2bd17afc2077342986ac3
Hugh Graham <hugh.a.graham@gmail.com>
2024-09-09 17:07:42 +0100

    drop extra vars

--- R/chewie-convert.r
+++ R/chewie-convert.r
@@ -15,56 +15,51 @@ chewie_convert.chewie.default <- function(x, ...) {
 }
 
 #' @export
-chewie_convert.chewie.download.1B <- function(x, extra_vars = NULL, ...) {
+chewie_convert.chewie.download.1B <- function(x, ...) {
   h5_to_gedi_dt(
     x,
     .f = l1b_h5_to_dt,
-    extra_vars,
     .progress = TRUE
   )
 }
 
-
-
 #' @export
-chewie_convert.chewie.download.2A <- function(x, extra_vars = NULL, ...) {
+chewie_convert.chewie.download.2A <- function(x, ...) {
   h5_to_gedi_dt(
     x,
     .f = l2a_h5_to_dt,
-    extra_vars,
     .progress = TRUE
   )
 }
 
 #' @export
-chewie_convert.chewie.download.2B <- function(x, extra_vars = NULL, ...) {
+chewie_convert.chewie.download.2B <- function(x, ...) {
   h5_to_gedi_dt(
     x,
     .f = l2b_h5_to_dt,
-    extra_vars,
     .progress = TRUE
   )
 }
 
 #' @export
-chewie_convert.chewie.download.4A <- function(x, extra_vars = NULL, ...) {
+chewie_convert.chewie.download.4A <- function(x, ...) {
   h5_to_gedi_dt(
     x,
     .f = l4a_h5_to_dt,
-    extra_vars,
     .progress = TRUE
   )
 }
 
 
-h5_to_gedi_dt <- function(x, .f, extra_vars, .progress = TRUE) {
+h5_to_gedi_dt <- function(x, .f, .progress = TRUE) {
   h5_open <- hdf5r::H5File$new(x$destfile, mode = "r")
   on.exit(h5_open$close())
   grps <- hdf5r::list.groups(h5_open, recursive = FALSE)
   beam_ids <- grps[startsWith(grps, "BEAM")]
 
-  purrr::map(beam_ids,
-    ~ .f(.x, h5_open, extra_vars),
+  purrr::map(
+    beam_ids,
+    ~ .f(.x, h5_open),
     .progress = .progress
   ) |>
     data.table::rbindlist()
