5b728bb86446e4e49ac20e757ce645c6ecd89bd5
Hugh Graham <hugh.a.graham@gmail.com>
2024-09-13 15:53:50 +0100

    improved test setup and teardown

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,27 +1,37 @@
-# --- Setup the testing environment.
-options("chewie.testing" = TRUE)
-temp_dir <- tempdir()
-setwd(temp_dir)
-if (!file.exists(".Renviron")) {
-  file.create(".Renviron")
-}
-
-cache_zip <- system.file(
-  "chewie-test-cache.zip",
-  package = "chewie"
-)
-
-utils::unzip(
-  cache_zip,
-  exdir = temp_dir
-)
-temp_cache <- file.path(temp_dir, "chewie-test-cache")
-
-chewie::chewie_setup_cache(temp_cache, renviron = "project")
-
 # --- Define the testing area.
 pcreek <- system.file(
   "geojson", "prairie-creek.geojson",
   package = "chewie"
 ) |>
   sf::read_sf()
+
+# --- Setup the testing environment.
+temp_dir <- tempdir()
+old_chewie_cache <- chewie::chewie_get_cache()
+options("chewie.testing" = TRUE)
+
+file.create(".Renviron")
+
+cache_zip <- system.file(
+  "chewie-test-cache.zip",
+  package = "chewie"
+)
+
+unzipped_files <- utils::unzip(
+  cache_zip,
+  exdir = temp_dir
+)
+
+chewie::chewie_setup_cache(file.path(temp_dir, "chewie-test-cache"),
+  renviron = "project"
+)
+# --- teardown section - tidying up
+withr::defer(
+  {
+    chewie::chewie_setup_cache(old_chewie_cache)
+    fs::file_delete(unzipped_files)
+    options(chewie.testing = NULL)
+    fs::file_delete(".Renviron")
+  },
+  envir = teardown_env()
+)
